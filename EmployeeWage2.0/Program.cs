ffc145f283604175b0b4f83881dab653bb4a409e
patelaftab <10patelaftab@gmail.com>
2022-12-25 18:14:54 +0530

    [Aftab] Welcome Message

---
+++ EmployeeWage2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2._0
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Program");
+            Console.ReadLine();
+        }
+    }
+}
