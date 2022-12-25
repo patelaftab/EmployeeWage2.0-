00c604e9d139eeb8c22212c6c00bf1a5329828ba
patelaftab <10patelaftab@gmail.com>
2022-12-25 18:35:33 +0530

    [Aftab] Calculating Daily Wage For An Employee

--- EmployeeWage2.0/EmployeeWage.cs
+++ EmployeeWage2.0/EmployeeWage.cs
@@ -11,16 +11,23 @@ namespace EmployeeWage2._0
         public void  chekingattendance()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random= new Random();
             int empcheck = random.Next(0,2);
             if (empcheck == IS_FULL_TIME)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee Is Present");
             }
             else 
             {
+                empHrs = 0;
                 Console.WriteLine("Employee Is Absent");
             }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee Daily Wage is : " + empWage);
         }
          
 
