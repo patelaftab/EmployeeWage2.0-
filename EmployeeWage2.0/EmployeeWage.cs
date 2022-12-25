4dc942141eef9bc3880b3d3efe5e8121f294942f
patelaftab <10patelaftab@gmail.com>
2022-12-25 18:41:08 +0530

    [Aftab] Adding Employee With Part Time Wage

--- EmployeeWage2.0/EmployeeWage.cs
+++ EmployeeWage2.0/EmployeeWage.cs
@@ -11,17 +11,23 @@ namespace EmployeeWage2._0
         public void  chekingattendance()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random= new Random();
-            int empcheck = random.Next(0,2);
+            int empcheck = random.Next(0,3);
             if (empcheck == IS_FULL_TIME)
             {
                 empHrs = 8;
                 Console.WriteLine("Employee Is Present");
             }
-            else 
+            else if(empcheck==IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("EMployee is Part Time");
+            }
+            else
             {
                 empHrs = 0;
                 Console.WriteLine("Employee Is Absent");
