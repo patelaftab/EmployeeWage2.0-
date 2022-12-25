d181c3f81e4b8476b3ee3174df8d530bf56ba4db
patelaftab <10patelaftab@gmail.com>
2022-12-25 21:15:59 +0530

    [Aftab] Checking Interface approach

---
+++ EmployeeWage2.0/IcomputeWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2._0
+{
+    internal interface IComputeEmpWage
+    {
+        public void AddCompanyEmpWage(string company, int empRateperHour, int numOfWorkingDays, int maxHoursPermonth);
+        public void ComputeEmpWage();
+    }
+}
